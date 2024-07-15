ad721dbb3571ee16bf1f1bc982477bf796704a9a
Faizanriaz44553 <sylanipractice@gmail.com>
2024-07-15 06:08:48 -0700

    project initialization

--- login.js
+++ login.js
@@ -13,34 +13,67 @@ const login = (event) => {
 
     // Get values from localStorage
     let signupdata = JSON.parse(localStorage.getItem("data"));
-    console.log(signupdata);
+     
 
     // Get error display element
     let error = document.querySelector('#validation-2');
 
-
-            if (usera === signupdata.user1) {
-                if (userb === signupdata.user2) {
-                    console.log("Correct Password");
-                Swal.fire({
-                    position: "top-center",
-                    icon: "success",
-                    title: "Succesfully Login",
-                    showConfirmButton: false,
-                    timer: 1500
-                });
-                setTimeout(function () {
-                    window.location.href = "./dash.html";
-                }, 1500);
-                } else {
-                    console.log("Incorrect password");
-                error.innerHTML = "Incorrect password";
-                error.className = "error";
-                }
-            } else {
-                console.log("Incorrect email");
-                error.innerHTML = "Incorrect email";
-                error.className = "error";
-            }
+    let compareEmail = signupdata.find((item)=>{
+        return item.user1  === usera;
+    })       
+    let comparePassword = signupdata.find((item)=>{
+        return item.user2 === userb;
+    })   
+    if (usera === "sylanipractice@gmail.com" && userb === "faizan") {
+        setTimeout(function () {
+            window.location.href = "./admin.html";
+        }, 1500);
+    }    
+    else if (compareEmail) {
+       if (comparePassword) {
+        console.log("Correct Password");
+        Swal.fire({
+            position: "top-center",
+            icon: "success",
+            title: "Succesfully Login",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        setTimeout(function () {
+            window.location.href = "./dash.html";
+        }, 1500);
+       } else {
+        console.log("Incorrect email");
+        error.innerHTML = "Incorrect Password";
+        error.className = "error";
+       }
+    } else {
+        console.log("Incorrect email");
+        error.innerHTML = "Incorrect email";
+        error.className = "error";
+    }
+         // if (usera === signupdata.user1) {
+            //     if (userb === signupdata.user2) {
+                //     console.log("Correct Password");
+                // Swal.fire({
+                //     position: "top-center",
+                //     icon: "success",
+                //     title: "Succesfully Login",
+                //     showConfirmButton: false,
+                //     timer: 1500
+                // });
+                // setTimeout(function () {
+                //     window.location.href = "./dash.html";
+                // }, 1500);
+            //     } else {
+            //         console.log("Incorrect password");
+            //     error.innerHTML = "Incorrect password";
+            //     error.className = "error";
+            //     }
+            // } else {
+                // console.log("Incorrect email");
+                // error.innerHTML = "Incorrect email";
+                // error.className = "error";
+            // }
 };
 
--- signupp.js
+++ signupp.js
@@ -2,46 +2,38 @@ let usera = document.querySelector('#user1')
 let userb = document.querySelector('#user2')
 let userc =document.querySelector("#user3")
 let profile =document.querySelector("#pic")
-
+let AllUserData = JSON.parse(localStorage.getItem('data')) || [];
 
 
 function validationForm() {
     event.preventDefault() 
-  let obj = {
-    user1: usera.value,
-    user2: userb.value,
-    user3: userc.value,
-    user4: profile.value,
-  }
-  const {user1 , user2 , user3 ,} = obj;
-  console.log(profile);
   let error = document.querySelector('#validation-2') 
   let CapitalLetter = false 
   let SmallLetter = false 
   let hasNumber = false; 
   let hasSpecialChar = false; 
   let specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '"', "'", '<', '>', ',', '.', '?', '/'];
-  if (user1.trim() === "") { 
+  if (usera.value.trim() === "") { 
     console.log("input Empity"); 
     error.innerHTML = ("Email is Empity"); 
     error.className = "error"; } 
     
-    else if (user2.trim() === "") { 
+    else if (userb.value.trim() === "") { 
       error.innerHTML = "Password is Empity" } 
      
-    else if (user1.length < 13) { 
-      error.innerHTML = "Please enter your correct Emai" } 
-      else { if (user2.length > 8) { 
-        for (let i = 0; i < user2.length; i++) {
-          if (user2[i] >= "A" && user2[i] <= "Z") { 
+    else if (usera.value.length < 13) { 
+      error.innerHTML = "Please enter your correct Email" } 
+      else { if (userb.value.length > 8) { 
+        for (let i = 0; i < userb.value.length; i++) {
+          if (userb.value[i] >= "A" && userb.value[i] <= "Z") { 
             CapitalLetter = true }; 
-            if (user2[i] >= "a" && user2[i] <= "z") { 
+            if (userb.value[i] >= "a" && userb.value[i] <= "z") { 
             SmallLetter = true 
             }; 
-            if (user2[i] >= "0" && user2[i] <= "9") {
+            if (userb.value[i] >= "0" && userb.value[i] <= "9") {
             hasNumber = true; 
             } 
-            if (specialChars.includes(user2[i])) {
+            if (specialChars.includes(userb.value[i])) {
             hasSpecialChar = true; 
             } 
         }
@@ -66,38 +58,45 @@ function validationForm() {
             error.className = "error";
         }
         if (CapitalLetter && SmallLetter && hasNumber && hasSpecialChar) {
-          // localStorage.setItem('data', JSON.stringify(obj));
-          //   console.log("Correct Password");
-          //   Swal.fire({
-          //   position: "top-center",
-          //   icon: "success",
-          //   title: "Login",
-          //   showConfirmButton: false,
-          //   timer: 1500
-          //   });
-          //  setTimeout(function() {
-          //       window.location.href = "./login.html";
-          //   } , 1000);
-           if (profile.files.length > 0) {
-                    let image = profile.files[0];
-                    let fileread = new FileReader();
-                    fileread.onload = () => {
-                        obj.profile = fileread.result;
-                        localStorage.setItem('data', JSON.stringify(obj));
-                        console.log("Correct Password");
-                        Swal.fire({
-                            position: "top-center",
-                            icon: "success",
-                            title: "Signup Successful",
-                            showConfirmButton: false,
-                            timer: 1500
-                        });
-                        setTimeout(function() {
-                            window.location.href = "./login.html";
-                        }, 1000);
+                        let alreadyAccout = AllUserData.find((item)=>{
+                          return item.user1 === usera.value;
+                        })
+                        console.log(alreadyAccout)
+                        if (alreadyAccout) {
+                          setTimeout(function () {
+                            window.location.href = "./dash.html";
+                        }, 1500);
+                        }
+                        else{
+                          if (profile.files.length > 0) {
+                            let image = profile.files[0];
+                            let fileread = new FileReader();
+                            fileread.onload = () => {
+                                obj.profile = fileread.result;
+                            }
+                            fileread.readAsDataURL(image);
+                          }
+                          let obj = {
+                            user1: usera.value,
+                            user2: userb.value,
+                            user3: userc.value,
+                            user4: profile.value,
+                          }
+                          AllUserData.push(obj)
+                          localStorage.setItem('data', JSON.stringify(AllUserData));
+                          console.log("Correct Password");
+                          Swal.fire({
+                              position: "top-center",
+                              icon: "success",
+                              title: "Signup Successful",
+                              showConfirmButton: false,
+                              timer: 1500
+                          });
+                          setTimeout(function() {
+                              window.location.href = "./login.html";
+                          }, 1000);
+                        }
                     }
-                    fileread.readAsDataURL(image);
-        }
     }
     else {
         console.log("Password atleast 8 charector valid");
@@ -106,15 +105,3 @@ function validationForm() {
     }
 }
 }
-}
-// const uploadimg = ()=>{
-//   let img = profile.files[0];
-//   let fileread = new FileReader();
-//   fileread.onload = ()=>{
-//     let imageobj = {
-//       profile : fileread.result
-//     }
-//     localStorage.set("imgData" , JSON.stringify(imageobj))
-//   }
-//   fileread.readAsDataURL(img)
-// }
